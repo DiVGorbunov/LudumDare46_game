8379c45575e3d9a51c1f8ee9f8e3d7467e098f0e
Sanyo <sanyo.gorbunov@gmail.com>
2020-04-18 16:04:08 +0200

    Client positions are withing surface

--- Assets/Sandbox/ClientGenerator.cs
+++ Assets/Sandbox/ClientGenerator.cs
@@ -3,16 +3,16 @@
 public class ClientGenerator : MonoBehaviour
 {
     public int number = 10;
-    public Transform prefab;
+    public Collider client;
     public BoxCollider surface;
     void Start()
     {
         for (int i = 0; i < number; i++)
         {
-            Instantiate(prefab,
-                new Vector3(Random.value * surface.bounds.size.x - surface.bounds.size.x / 2f,
+            Instantiate(client,
+                new Vector3(surface.center.x + (Random.value - 0.5f) * (surface.bounds.size.x - client.bounds.size.x),
                     1.5f,
-                    Random.value * surface.bounds.size.z - surface.bounds.size.z / 2f), Quaternion.identity);
+                    surface.center.z + (Random.value - 0.5f) * (surface.bounds.size.z - client.bounds.size.z)), Quaternion.identity);
         }
     }
 }
