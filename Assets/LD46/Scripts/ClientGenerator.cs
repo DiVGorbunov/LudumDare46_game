72973c17efc21104092a534bc2bf3ed0d6144b8e
Sanyo <sanyo.gorbunov@gmail.com>
2020-04-18 19:34:28 +0200

    Merged

--- Assets/LD46/Scripts/ClientGenerator.cs
+++ Assets/LD46/Scripts/ClientGenerator.cs
@@ -8,37 +8,44 @@ public class ClientGenerator : MonoBehaviour
     public Vector3 clientTransform = new Vector3(1f, 1f, 1f);
 
     private const string ClientTag = "Client";
+    private const int PlacementAttempts = 5;
 
     void Start()
     {
-        var overlapRadius = clientTransform.y * 2 + 0.5f;
-
         for (int i = 0; i < number; i++)
         {
-            var newClientPosition = GetNewClientPosition(overlapRadius);
-            var newClient = Instantiate(client, newClientPosition, Quaternion.identity);
-            newClient.transform.localScale = clientTransform;
-            newClient.tag = ClientTag;
+            Vector3 newClientPosition;
+            if (TryGetNewClientPosition(out newClientPosition))
+            {
+                var newClient = Instantiate(client, newClientPosition, Quaternion.identity);
+                newClient.transform.localScale = clientTransform;
+                newClient.tag = ClientTag;
+            }
         }
     }
 
-    private Vector3 GetNewClientPosition(float overlapRadius)
+    private bool TryGetNewClientPosition(out Vector3 position)
     {
+        int attempts = 0;
         do
         {
             var surface = GetRandomSurface();
-            var position = new Vector3(surface.bounds.center.x + (Random.value - 0.5f) * (surface.bounds.size.x - clientTransform.x),
+            position = new Vector3(surface.bounds.center.x + (Random.value - 0.5f) * (surface.bounds.size.x - clientTransform.x),
                    surface.bounds.center.y + surface.bounds.extents.y + clientTransform.y,
                    surface.bounds.center.z + (Random.value - 0.5f) * (surface.bounds.size.z - clientTransform.z));
 
-            var overlaps = Physics.OverlapCapsule(position, clientTransform, overlapRadius);
+            var overlaps = Physics.OverlapCapsule(position, clientTransform, clientTransform.y / 2f);
             if (HasClient(overlaps))
             {
+                attempts++;
                 continue;
             }
 
-            return position;
-        } while (true);
+            return true;
+        } while (attempts < PlacementAttempts);
+
+        position = Vector3.zero;
+        return false;
     }
 
     private BoxCollider GetRandomSurface()
