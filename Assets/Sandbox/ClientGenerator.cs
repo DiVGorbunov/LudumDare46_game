22ebe6642c34765065885ebf04d67ec762cf8fcd
Sanyo <sanyo.gorbunov@gmail.com>
2020-04-18 17:16:43 +0200

    Removed overlaps

--- Assets/Sandbox/ClientGenerator.cs
+++ Assets/Sandbox/ClientGenerator.cs
@@ -7,18 +7,38 @@ public class ClientGenerator : MonoBehaviour
     public BoxCollider[] surfaces;
     public Vector3 clientTransform = new Vector3(1f, 1f, 1f);
 
+    private const string ClientTag = "Client";
+
     void Start()
     {
+        var overlapRadius = clientTransform.y * 2 + 0.5f;
+
         for (int i = 0; i < number; i++)
         {
-            var surface = GetRandomSurface();
-
-            var newClient = Instantiate(client,
-                new Vector3(surface.bounds.center.x + (Random.value - 0.5f) * (surface.bounds.size.x - clientTransform.x),
-                    surface.bounds.center.y + surface.bounds.extents.y + clientTransform.y,
-                    surface.bounds.center.z + (Random.value - 0.5f) * (surface.bounds.size.z - clientTransform.z)), Quaternion.identity);
+            var newClientPosition = GetNewClientPosition(overlapRadius);
+            var newClient = Instantiate(client, newClientPosition, Quaternion.identity);
             newClient.transform.localScale = clientTransform;
+            newClient.tag = ClientTag;
         }
+    }
+
+    private Vector3 GetNewClientPosition(float overlapRadius)
+    {
+        do
+        {
+            var surface = GetRandomSurface();
+            var position = new Vector3(surface.bounds.center.x + (Random.value - 0.5f) * (surface.bounds.size.x - clientTransform.x),
+                   surface.bounds.center.y + surface.bounds.extents.y + clientTransform.y,
+                   surface.bounds.center.z + (Random.value - 0.5f) * (surface.bounds.size.z - clientTransform.z));
+
+            var overlaps = Physics.OverlapCapsule(position, clientTransform, overlapRadius);
+            if (HasClient(overlaps))
+            {
+                continue;
+            }
+
+            return position;
+        } while (true);
     }
 
     private BoxCollider GetRandomSurface()
@@ -33,5 +53,18 @@ public class ClientGenerator : MonoBehaviour
         }
 
         return surfaces[i];
+    }
+
+    private bool HasClient(Collider[] overlaps)
+    {
+        for (int i = 0; i < overlaps.Length; i++)
+        {
+            if (overlaps[i].gameObject.CompareTag(ClientTag))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
