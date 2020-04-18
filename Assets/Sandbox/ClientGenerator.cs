5d8fe50e644fa4d410087f3115d27a31ea9802f2
Sanyo <sanyo.gorbunov@gmail.com>
2020-04-18 16:37:13 +0200

    Relied on client size

--- Assets/Sandbox/ClientGenerator.cs
+++ Assets/Sandbox/ClientGenerator.cs
@@ -3,16 +3,19 @@
 public class ClientGenerator : MonoBehaviour
 {
     public int number = 10;
-    public Collider client;
+    public GameObject client;
     public BoxCollider surface;
+    public Vector3 clientTransform = new Vector3(1f, 1f, 1f);
+
     void Start()
     {
         for (int i = 0; i < number; i++)
         {
-            Instantiate(client,
-                new Vector3(surface.center.x + (Random.value - 0.5f) * (surface.bounds.size.x - client.bounds.size.x),
-                    1.5f,
-                    surface.center.z + (Random.value - 0.5f) * (surface.bounds.size.z - client.bounds.size.z)), Quaternion.identity);
+            var newClient = Instantiate(client,
+                new Vector3(surface.center.x + (Random.value - 0.5f) * (surface.bounds.size.x - clientTransform.x),
+                    surface.center.y + surface.bounds.extents.y + clientTransform.y,
+                    surface.center.z + (Random.value - 0.5f) * (surface.bounds.size.z - clientTransform.z)), Quaternion.identity);
+            newClient.transform.localScale = clientTransform;
         }
     }
 }
