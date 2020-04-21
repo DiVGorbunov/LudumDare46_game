dd6386fc7b339d5d1f64e2c5da70174a5d937f76
Sanyo <sanyo.gorbunov@gmail.com>
2020-04-21 02:57:04 +0200

    Added more levels

--- Assets/LD46/Scripts/DifficultyManager.cs
+++ Assets/LD46/Scripts/DifficultyManager.cs
@@ -16,7 +16,7 @@
 
     public int CurrentLevel { get; private set; }
 
-    public bool IsMaxLevel => CurrentLevel == 4;
+    public bool IsMaxLevel => CurrentLevel == 10;
 
     private DifficultyManager()
     {
@@ -38,11 +38,23 @@
             case 0:
                 return new[] { 1f, 0f, 0f};
             case 1:
-                return new[] { 0.5f, 0.5f, 0f };
+                return new[] { 0.8f, 0.2f, 0f };
             case 2:
-                return new[] { 0.34f, 0.33f, 0.33f };
+                return new[] { 0.6f, 0.4f, 0f };
             case 3:
+                return new[] { 0.5f, 0.5f, 0f };
+            case 4:
+                return new[] { 0.4f, 0.4f, 0.2f };
+            case 5:
+                return new[] { 0.2f, 0.4f, 0.4f };
+            case 6:
+                return new[] { 0.0f, 0.75f, 0.25f };
+            case 7:
                 return new[] { 0f, 0.5f, 0.5f };
+            case 8:
+                return new[] { 0f, 0.3f, 0.7f };
+            case 9:
+                return new[] { 0f, 0f, 1f };
             default:
                 return new[] { 0f, 0.0f, 1f };
         }
@@ -55,13 +67,25 @@
             case 0:
                 return 5;
             case 1:
-                return 7;
+                return 5;
             case 2:
-                return 7;
+                return 5;
             case 3:
-                return 12;
+                return 10;
+            case 4:
+                return 10;
+            case 5:
+                return 14;
+            case 6:
+                return 14;
+            case 7:
+                return 20;
+            case 8:
+                return 20;
+            case 9:
+                return 20;
             default:
-                return 12;
+                return 20;
         }
     }
 
@@ -70,15 +94,27 @@
         switch (CurrentLevel)
         {
             case 0:
-                return 2;
+                return 1;
             case 1:
-                return 4;
+                return 1;
             case 2:
-                return 4;
+                return 2;
             case 3:
-                return 6;
+                return 2;
+            case 4:
+                return 3;
+            case 5:
+                return 3;
+            case 6:
+                return 4;
+            case 7:
+                return 4;
+            case 8:
+                return 4;
+            case 9:
+                return 4;
             default:
-                return 6;
+                return 4;
         }
     }
 
@@ -87,13 +123,25 @@
         switch (CurrentLevel)
         {
             case 0:
-                return 0;
+                return 1;
             case 1:
-                return 3;
+                return 1;
             case 2:
-                return 2;
+                return 1;
             case 3:
                 return 1;
+            case 4:
+                return 4;
+            case 5:
+                return 4;
+            case 6:
+                return 3;
+            case 7:
+                return 2;
+            case 8:
+                return 2;
+            case 9:
+                return 2;
             default:
                 return 1;
         }
@@ -111,8 +159,20 @@
                 return ( 45f, 5f);
             case 3:
                 return ( 43f, 5f);
+            case 4:
+                return (45f, 5f);
+            case 5:
+                return (45f, 5f);
+            case 6:
+                return (45f, 5f);
+            case 7:
+                return (45f, 5f);
+            case 8:
+                return (45f, 5f);
+            case 9:
+                return (45f, 5f);
             default:
-                return ( 40f, 3f);
+                return (45f, 5f);
         }
     }
 }
